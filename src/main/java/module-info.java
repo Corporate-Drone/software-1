b5179cb230a48ceb0c49f8f257ab88bbe0efa4f5
D Marcus <mindlesscorporatedrone@gmail.com>
2022-10-23 13:36:32 -0700

    initialize project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module dms.c482 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens dms.c482 to javafx.fxml;
+    exports dms.c482;
+    exports controller;
+    opens controller to javafx.fxml;
+}
