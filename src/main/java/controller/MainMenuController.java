def3b8432ec4c4594ecb776eb9b9094bfa00f056
D Marcus <mindlesscorporatedrone@gmail.com>
2022-10-23 15:25:13 -0700

    added Main Menu scene

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainMenuController {
+
+    @FXML
+    void onActionExit(ActionEvent event) {
+        System.exit(0);
+    }
+
     @FXML
     private Label welcomeText;
 
